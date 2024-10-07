9931b1d03e0941911a8961953bffa3ee39de284f
Aseel-Sh <aseel.sh52@gmail.com>
2024-10-06 21:40:19 -0500

    Added Shopping Cart Model

--- Bulky.DataAccess/Repository/ApplicationUserRepository.cs
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -10,17 +10,12 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
     {
         private readonly ApplicationDbContext _db;
-        public ShoppingCartRepository(ApplicationDbContext db) : base(db) 
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db) 
         {
             _db = db;
-        }
-
-        public void Update(ShoppingCart obj)
-        {
-            _db.ShoppingCarts.Update(obj);
         }
     }
 }
