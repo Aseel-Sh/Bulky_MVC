d58cd93739bb1b3f707dcd7bb4b3150218fe0772
Aseel Sh <aseel.sh52@gmail.com>
2024-11-16 20:01:18 -0600

    Added some things to shopping cart

---
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVMroperty { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;  
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimsTypes.NameIdentifier).Value;
+            return View();
+        }
+    }
+}
