bc2a6ed72c5d47bea87381f42904be049f98bde9
Aseel-Sh <aseel.sh52@gmail.com>
2024-12-17 14:10:14 -0600

    Temporary commit before running git-filter-repo

---
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using BulkyBook.DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
+        public void Initialize()
+        {
+            //push migrations if not applied
+
+            //create role if not created
+
+            //if roles not created then create admin user
+        }
+    }
+}
