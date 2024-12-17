d36d37cfbabfcaeafea9ccf7447008521a573a8e
Aseel-Sh <aseel.sh52@gmail.com>
2024-12-17 16:53:10 -0600

    Added a db initializer

--- Bulky.DataAccess/DbInitializer/DbInitializer.cs
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -1,5 +1,8 @@
 ﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,11 +27,46 @@ namespace BulkyBook.DataAccess.DbInitializer
         }
         public void Initialize()
         {
+
             //push migrations if not applied
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0) 
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
 
             //create role if not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
 
-            //if roles not created then create admin user
+                //if roles not created then create admin user
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "aseel.sh52@gmail.com",
+                    Email = "aseel.sh52@gmail.com",
+                    Name = "Aseel Shaheen",
+                    PhoneNumber = "1112223333",
+                    StreetAddress = "test 123 Ave",
+                    State = "IL",
+                    PostalCode = "23422",
+                    City = "Chicago"
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "aseel.sh52@gmail.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+
+            return;
+
         }
     }
 }
